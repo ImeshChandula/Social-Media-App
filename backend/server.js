667c804de019d19cd6e0c91d30da766d61bc5472
Imesh Chandula <117582263+ImeshChandula@users.noreply.github.com>
2025-04-24 13:04:21 +0530

    setup DB

--- backend/server.js
+++ backend/server.js
@@ -3,16 +3,25 @@ const dotenv = require("dotenv");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const connectDB = require("./src/config/db");
+const createDefaultSuperAdmin = require("./src/config/defaultSuperAdmin");
 
 dotenv.config();
 connectDB();
 
 const app = express();
-app.use(express.json());
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(express.json()); // starts the server
+
+// functions on server start
+//createDefaultSuperAdmin();
+
+app.use(cors({ origin: "http://localhost:4000", credentials: true }));
 app.use(cookieParser());
 
-//Routes
+// Routes
+// http://localhost:5000
+
+
+
 
 app.get("/", (req, res) => {
   res.send("API is running...");
