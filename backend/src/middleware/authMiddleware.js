02b3ff8e98807f5a2bd5af188d2a26eb7710bdae
DimithraLakshan <dimithralakshan017@mail.com>
2025-04-24 16:18:58 +0530

    authentication

---
+++ backend/src/middleware/authMiddleware.js
@@ -0,0 +1,36 @@
+const jwt = require("jsonwebtoken");
+
+const authenticateUser = (req, res, next) => {
+  const authHeader = req.header("Authorization");
+
+  if (!authHeader || !authHeader.startsWith("Bearer ")){
+    return res.status(401).json({ message: "No token, Authorization Access Denied" });
+  } 
+
+  const token = authHeader.split(" ")[1]; // Extract token after "Bearer"
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+
+    console.log("Authenticated User:", req.user);
+
+    next();
+  } catch (error) {
+    res.status(400).json({ message: "Invalid Token" });
+  }
+};
+
+
+
+
+const authorizeRoles = (...allowedRoles) => {
+  return (req, res, next) => {
+    if (!req.user || !allowedRoles.includes(req.user.role)) {
+      return res.status(403).json({ message: "Access Forbidden" });
+    }
+    next();
+  };
+};
+
+module.exports = { authenticateUser, authorizeRoles };
