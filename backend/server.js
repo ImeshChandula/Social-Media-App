9941fcf77077573736f7f1d7bd0ee1b7ff14d118
Imesh Chandula <117582263+ImeshChandula@users.noreply.github.com>
2025-05-06 20:32:12 +0530

    done

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ initializeDefaultSuperAdmin().then(() => {
 // starts the server
 const app = express();
 app.use(express.json()); 
-app.use(cors({ origin: "http://localhost:4000", credentials: true }));
+app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));
 app.use(cookieParser());
 
 
