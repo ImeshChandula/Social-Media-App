7a79fcec7f19fabb33870d11a4f32761c5a02f36
Imesh Chandula <117582263+ImeshChandula@users.noreply.github.com>
2025-05-15 14:20:29 +0530

    add auth store

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { useAuthStore } from './store/authStore';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 
@@ -8,18 +9,35 @@ import Register from "./routes/Register";
 import MainLayout from "./routes/MainLayout";
 
 function App() {
+  /*
   const [isLoggedIn, setIsLoggedIn] = useState(() => {
     return localStorage.getItem("isLoggedIn") === "true";
   });
 
   useEffect(() => {
     localStorage.setItem("isLoggedIn", isLoggedIn ? "true" : "false");
-  }, [isLoggedIn]);
+  }, [isLoggedIn]);*/
+
+  const {authUser, checkAuth, isCheckingAuth} = useAuthStore();
+
+  useEffect(() => {
+    checkAuth();
+  }, [checkAuth]);
+
+  console.log({ authUser });
+
+  if (isCheckingAuth && !authUser) 
+    return (
+      <div className='loader-container'>
+        <Loader className="loader" />
+      </div>
+    )
+
 
   return (
     <Router>
       <Routes>
-        {!isLoggedIn ? (
+        {/*!isLoggedIn ? (
           <>
             <Route path="/" element={<Login setIsLoggedIn={setIsLoggedIn} />} />
             <Route path="/register" element={<Register />} />
@@ -29,7 +47,16 @@ function App() {
           <>
             <Route path="/*" element={<MainLayout />} />
           </>
-        )}
+        )*/}
+
+        
+          <Route path="/" element={!authUser ? <Login /> : <Navigate to="/*" />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
+          
+          <Route path="/*" element={<MainLayout />} />
+          
+        
       </Routes>
     </Router>
   );
