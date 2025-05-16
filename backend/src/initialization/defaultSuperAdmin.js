0b68c65df2bcd77cdedc35084c2886f34892ae51
Imesh Chandula <117582263+ImeshChandula@users.noreply.github.com>
2025-05-16 10:24:16 +0530

    modify

--- backend/src/initialization/defaultSuperAdmin.js
+++ backend/src/initialization/defaultSuperAdmin.js
@@ -9,21 +9,27 @@ const createDefaultSuperAdmin = async (req, res) => {
         
         
         if (!existingSuperAdmin) {
-            // user data object
-            const superAdmin = new User({
+            // generate a num between 1-100
+            const index = Math.floor(Math.random() * 100) + 1;
+            const randomAvatar = `https://avatar.iran.liara.run/public/${index}.png`;
+            const defaultCover = process.env.DEFAULT_COVER_PHOTO || "https://static.cognitoforms.com/website/assets/default-video-cover-photo.Djn4Ebbl.png";
+        
+            const superAdminData = {
                 username: process.env.SUPER_ADMIN_USER_NAME,
                 email: process.env.SUPER_ADMIN_EMAIL,
                 password: process.env.SUPER_ADMIN_PASSWORD,
                 firstName: process.env.SUPER_ADMIN_FIRST_NAME,
                 lastName: process.env.SUPER_ADMIN_LAST_NAME,
                 role: "super_admin",
-            });
+                profilePicture: randomAvatar,
+                coverPhoto: defaultCover
+            };
     
-            await superAdmin.save();
-            console.log("Default Super Admin created: \nEmail: superadmin@test.lk \nPassword: super123");
+            await User.create(superAdminData);
+            console.log(`Default Super Admin created: \nEmail: ${process.env.SUPER_ADMIN_EMAIL} \nPassword: ${process.env.SUPER_ADMIN_PASSWORD}`);
         } else {
             superAdmin = existingSuperAdmin;
-            console.log("Default Super Admin already exists. \nEmail: superadmin@test.lk \nPassword: super123");
+            console.log(`Default Super Admin already exists. \nEmail: ${process.env.SUPER_ADMIN_EMAIL} \nPassword: ${process.env.SUPER_ADMIN_PASSWORD}`);
         }
     } catch (err) {
         console.error(err.message);
