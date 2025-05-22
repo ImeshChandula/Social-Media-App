1da252b2fa0ae9a032aec0cb3bd474422d7ef5e9
Imesh Chandula <117582263+ImeshChandula@users.noreply.github.com>
2025-05-22 21:10:00 +0530

    updated

--- backend/server.js
+++ backend/server.js
@@ -35,7 +35,8 @@ socketHandler(io);
 // Make io accessible to other files - ADD THIS
 global.io = io;
 
-app.use(express.json()); 
+app.use(express.json({ limit: '15mb' })); 
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
 app.use(cors({ origin: process.env.FRONTEND_URL || "http://localhost:4000", credentials: true }));
 app.use(cookieParser());
 
