0dbded2dbded97a726b298d54a0c2ec409367423
Pasindu Hirumal <pasinduhirumal7@gmail.com>
2025-06-17 16:58:08 +0530

    user profile done

--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-// eslint-disable-next-line no-unused-vars
 import { motion } from "framer-motion";
 import { axiosInstance } from "../lib/axios";
 import { useNavigate } from "react-router-dom";
