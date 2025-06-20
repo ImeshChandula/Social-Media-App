1db3af13743c745054f5065ad030123477f3ce09
Imesh Chandula <117582263+ImeshChandula@users.noreply.github.com>
2025-06-20 12:09:04 +0530

    admin dashboard

--- frontend/src/components/DashboardHome.jsx
+++ frontend/src/components/DashboardHome.jsx
@@ -3,6 +3,7 @@ import styles from '../styles/DashboardHomeStyle';
 import "../styles/DashboardHome.css";
 import DashboardUserSummery from './DashboardUserSummery';
 import TicketsHead from './TicketsHead';
+import MailHead from './MailHead';
 
 
 const DashboardHome = () => {
@@ -12,7 +13,7 @@ const DashboardHome = () => {
           <h2 style={styles.heading}>Dashboard Overview</h2>
           <p style={styles.paragraph}>Use the sidebar to navigate through admin options.</p>
       </div>
-      
+
       <TicketsHead />
       <DashboardUserSummery />
       
