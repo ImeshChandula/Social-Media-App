f48aa8957f4ca3ebad34f0aeb175d25f1a943364
Imesh Chandula <117582263+ImeshChandula@users.noreply.github.com>
2025-06-20 17:01:46 +0530

    uncomment story component

--- frontend/src/components/RegularUserPage.jsx
+++ frontend/src/components/RegularUserPage.jsx
@@ -1,9 +1,10 @@
-import Feedstories from "./Feedstories"
+import FeedStories from "./FeedStories"
 import Feed from "./Feed";
 
 const RegularUserPage = () => {
     return (
         <div className="py-5 py-md-0 mt-5 mt-md-0">
+            <FeedStories />
             <Feed />
         </div>
     );
