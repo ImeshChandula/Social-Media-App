3be9de9d3805158ba568f7150034e1b6b4799314
M.K. Priyanka <121155062+PriyankaPriyadarshani1024@users.noreply.github.com>
2025-07-14 17:09:04 +0530

    update

--- frontend/src/components/RegularUserPage.jsx
+++ frontend/src/components/RegularUserPage.jsx
@@ -4,7 +4,7 @@ import Feed from "./Feed";
 const RegularUserPage = () => {
     return (
         <div className="py-5 py-md-0 mt-5 mt-md-0">
-            <FeedStories />
+            <FeedStories type="all" />
             <Feed />
         </div>
     );
