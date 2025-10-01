5326a7cde825ec98d64301a2594c115f3e729acc
Pasindu Hirumal <pasinduhirumal7@gmail.com>
2025-10-01 10:17:14 +0530

    updated

--- frontend/src/store/UselessTheme.js
+++ frontend/src/store/UselessTheme.js
@@ -6,9 +6,3 @@ const { isDarkMode } = useThemeStore();
     className={`container-fluid vh-100 overflow-hidden ${isDarkMode ? "" : "bg-gray-100 text-black"}`}
 >
 </div>
-
-{/* 
-    Prompt for chatGPT
-    
-    Use tailwindcss to only change the parts that are related to darkmode lightmod (text colors, background colors). Do not change any other design. Also, provide a toggle next to the search button.
-*/}
